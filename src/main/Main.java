7bb79001f6c0db245b80330b6ca493d4defb6a45
Nursultan <nursultan_imanov@mail.ru>
2018-09-27 22:05:42 +0200

    init

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame=new JFrame();
+		JButton b=new JButton("click");
+		frame.add(b);
+		frame.setSize(200,200);
+	}
+}
